2dd9705c7f3e6d54ab5b47bab91ac2f4210d7228
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-01-31 04:06:44 +0100

    update foundry

--- src/modules/GameMinter.sol
+++ src/modules/GameMinter.sol
@@ -82,14 +82,20 @@ contract GameMinter is ERC721("Connect 4", "CONN4"), StringUtils {
             );
         }
 
-        string memory encoded = Base64.encode(
+        string memory encoded = string(
             abi.encodePacked(
                 '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 600" stroke="none" width="350px"><defs>'
                 '<pattern id="cell-pattern" patternUnits="userSpaceOnUse" width="100" height="100"><circle cx="50" cy="50" r="45" fill="black"></circle></pattern>',
                 '<mask id="cell-mask"><rect width="100" height="600" fill="white"></rect><rect width="100" height="600" fill="url(#cell-pattern)"></rect></mask></defs>',
                 rows[0],
                 rows[1],
-                rows[2],
+                rows[2]
+            )
+        );
+
+        encoded = Base64.encode(
+            abi.encodePacked(
+                encoded,
                 rows[3],
                 rows[4],
                 rows[5],
